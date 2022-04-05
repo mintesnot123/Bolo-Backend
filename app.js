89c25ccfa3df4ff016ca0f1497bfddc52ccd20f2
BlueCloud15696 <webmaster15696@gmail.com>
2022-04-05 06:34:11 -0700

    Imported routes to app.js

--- app.js
+++ app.js
@@ -1,20 +1,49 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+let mongoose = require("mongoose");
+let multer = require("multer");
+let cors = require("cors");
 
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
+var authRouter = require("./routes/auth");
+var questionsRouter = require("./routes/question");
+var developersRouter = require("./routes/developer");
+
+const connectionString =
+    "mongodb+srv://user1:passwordv@cluster0.o6rah.mongodb.net/DatabaseName?retryWrites=true&w=majority";
+mongoose.connect(connectionString, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+});
 var app = express();
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+const db = mongoose.connection;
+db.on("error", (error) => {
+    console.log("error", error);
+});
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+db.once("open", async function () {
+    app.use(logger("dev"));
+    app.use(express.json());
+    app.use(cors());
+    app.use(express.static('public'));
+    app.use(express.urlencoded({ extended: false }));
+    app.use(cookieParser());
+    //app.use(express.static(path.join(__dirname, "public")));
+
+    app.use("/", indexRouter);
+    app.use("/api/auth", authRouter);
+    app.use("/api/users", usersRouter);
+    app.use("/api/questions", questionsRouter);
+    app.use("/api/developers", developersRouter);
+
+    app.listen(process.env.PORT || 4000, () =>
+        console.log("Listening 4000...")
+    );
+});
 
 module.exports = app;
