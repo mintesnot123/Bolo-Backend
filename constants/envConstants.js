96ca2f68718ceece60adef13a55c6540671306ce
BlueCloud15696 <webmaster15696@gmail.com>
2022-04-06 05:29:25 -0700

    credintials added

--- constants/envConstants.js
+++ constants/envConstants.js
@@ -2,8 +2,8 @@ const envConstants = {
     USER_VERIFICATION_TOKEN_SECRET: "dgfgpspdifgskdfngussj490385jsp8ms",
     secret: "gew67dfgew", //secret key
     TOKE_NAME: "accessTokenBolo",
-    email: "your email",
-    emailPass: "email password",
+    email: "yismawmintesnot@gmail.com",
+    emailPass: "twoanmsseitynim",
     BASE_URL: "https://localhost:3000",
 };
 
