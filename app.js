7febca4fa97888751814a9ad54484ece98e33942
mintesnot123 <yismawmintesnot@gmail.com>
2022-04-07 03:47:13 -0700

    static file access

--- app.js
+++ app.js
@@ -30,7 +30,8 @@ db.once("open", async function () {
     app.use(logger("dev"));
     app.use(express.json());
     app.use(cors());
-    app.use(express.static('public'));
+    app.use('/static', express.static(__dirname + '/public'));
+    //app.use(express.static('public'));
     app.use(express.urlencoded({ extended: false }));
     app.use(cookieParser());
     //app.use(express.static(path.join(__dirname, "public")));
