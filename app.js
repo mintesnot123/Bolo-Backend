80a1867446d72fe314c738e39ae1686229d7d2bf
mintesnot123 <yismawmintesnot@gmail.com>
2022-04-07 03:50:31 -0700

    upload folder added

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ db.once("open", async function () {
     app.use(logger("dev"));
     app.use(express.json());
     app.use(cors());
-    app.use('/static', express.static(__dirname + '/public'));
+    app.use('/static', express.static(__dirname + './public'));
     //app.use(express.static('public'));
     app.use(express.urlencoded({ extended: false }));
     app.use(cookieParser());
