34cef76872af3678ab8d9ab50ffcb422e3a6c4dd
BlueCloud15696 <webmaster15696@gmail.com>
2022-04-11 23:41:57 +0300

    edited: developers avatar folder from public to uploads

--- app.js
+++ app.js
@@ -30,7 +30,8 @@ db.once("open", async function () {
     app.use(logger("dev"));
     app.use(express.json());
     app.use(cors());
-    app.use(express.static('public'));
+    app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+    //app.use(express.static('public'));
     app.use(express.urlencoded({ extended: false }));
     app.use(cookieParser());
     //app.use(express.static(path.join(__dirname, "public")));
